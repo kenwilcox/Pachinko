05e5c4ceffb2cfe08627a1da1dfb011b4dab9f38
Ken Wilcox <wilcoxk@gmail.com>
2015-12-05 00:30:45 -0700

    Now it's a ball - should fix #5

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -24,10 +24,11 @@ class GameScene: SKScene {
     /* Called when a touch begins */
     if let touch = touches.first {
       let location = touch.locationInNode(self)
-      let box = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 64, height: 64))
-      box.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 64, height: 64))
-      box.position = location
-      addChild(box)
+      let ball = SKSpriteNode(imageNamed: "ballRed")
+      ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+      ball.physicsBody!.restitution = 0.4
+      ball.position = location
+      addChild(ball)
     }
   }
   
