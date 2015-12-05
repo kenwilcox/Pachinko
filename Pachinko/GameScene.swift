7e83dbb7e62948b4e088097df3e1235c948886a9
Ken Wilcox <wilcoxk@gmail.com>
2015-12-04 22:56:07 -0700

    Assets and test code

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -9,12 +9,26 @@
 import SpriteKit
 
 class GameScene: SKScene {
+  
   override func didMoveToView(view: SKView) {
     /* Setup your scene here */
+    let background = SKSpriteNode(imageNamed: "background.jpg")
+    background.position = CGPoint(x: 512, y: 384)
+    background.blendMode = .Replace
+    background.zPosition = -1
+    addChild(background)
+    physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
   }
   
   override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
     /* Called when a touch begins */
+    if let touch = touches.first {
+      let location = touch.locationInNode(self)
+      let box = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 64, height: 64))
+      box.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 64, height: 64))
+      box.position = location
+      addChild(box)
+    }
   }
   
   override func update(currentTime: CFTimeInterval) {
